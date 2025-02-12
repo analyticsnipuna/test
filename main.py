1870a0c439ecaec7cbd645cfc7f320a65a4804b7
analyticsnipuna <130767805+analyticsnipuna@users.noreply.github.com>
2025-02-12 21:59:56 +0900

    Update main.py
    font changed

--- main.py
+++ main.py
@@ -23,11 +23,17 @@ elif current_os == "Darwin":  # macOS
     # Mac 환경 폰트 설정
     plt.rcParams["font.family"] = "AppleGothic"
 else:  # Linux 등 기타 OS
-    # 기본 한글 폰트 설정 시도
-    try:
+    if "NanumGothic" in fm.findSystemFonts():
         plt.rcParams["font.family"] = "NanumGothic"
-    except:
-        print("한글 폰트를 찾을 수 없습니다. 시스템 기본 폰트를 사용합니다.")
+    else:
+        print("⚠️ NanumGothic 폰트를 찾을 수 없습니다. 시스템 기본 폰트 사용.")
+        plt.rcParams["font.family"] = "sans-serif"
+# else:  # Linux 등 기타 OS
+#     # 기본 한글 폰트 설정 시도
+#     try:
+#         plt.rcParams["font.family"] = "NanumGothic"
+#     except:
+#         print("한글 폰트를 찾을 수 없습니다. 시스템 기본 폰트를 사용합니다.")
 
 ##### 마이너스 폰트 깨짐 방지 #####
 plt.rcParams["axes.unicode_minus"] = False  # 마이너스 폰트 깨짐 방지
