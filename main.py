7d5b7450675c6878973c40ae212576165bbfe64c
analyticsnipuna <130767805+analyticsnipuna@users.noreply.github.com>
2025-03-11 22:29:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-st.title("NOVA Playgroud")
+st.title("NOVA's Playgroud")
 st.markdown(
     """
 1. data analysis agent
-2. PDF to Markdown File (w/ Upstage OCR)
-3. Prompt Maker
+2. (Coming Soon)
 """
 )
